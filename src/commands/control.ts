8b1ba61ec2f916ce10cf424e56ca52787969e4cd
Matthew Douglass <5410142+mdouglass@users.noreply.github.com>
2022-08-24 10:14:58 -0700

    Add commands for changing shuffle/repeat settings

--- src/commands/control.ts
+++ src/commands/control.ts
@@ -1,4 +1,5 @@
 import {Command, Flags} from '@oclif/core'
+import {Repeat} from '@svrooij/sonos/lib/models'
 import SonosCommandHelper from '../helpers/sonos-command-helper'
 
 export default class Control extends Command {
@@ -12,12 +13,27 @@ export default class Control extends Command {
   static args = [
     {name: 'device', required: true, description: 'Name or uuid of player'},
     {name: 'command', required: true, description: 'What command do you want to send',
-      options: ['play', 'pause', 'next', 'previous', 'toggle', 'stop', 'volumeup', 'volumedown', 'mute', 'unmute', 'togglemute']},
+      options: ['play', 'pause', 'next', 'previous', 'toggle', 'stop', 'volumeup', 'volumedown', 'mute', 'unmute', 'togglemute', 'repeatall', 'repeatone', 'repeatoff', 'togglerepeat', 'shuffleon', 'shuffleoff', 'toggleshuffle']},
   ]
 
   async run(): Promise<void> {
     const {args, flags} = await this.parse(Control)
     const device = await SonosCommandHelper.device(this, flags, args.device)
+
+    async function toggleRepeat(): Promise<void> {
+      const repeat = await device.GetRepeat()
+      switch (repeat) {
+      case Repeat.Off:
+        await device.SetRepeat(Repeat.RepeatAll)
+        break
+      case Repeat.RepeatAll:
+        await device.SetRepeat(Repeat.RepeatOne)
+        break
+      case Repeat.RepeatOne:
+        await device.SetRepeat(Repeat.Off)
+        break
+      }
+    }
 
     switch (args.command) {
     case 'play':
@@ -55,6 +71,28 @@ export default class Control extends Command {
       await device.RenderingControlService.SetMute({InstanceID: 0, Channel: 'Master', DesiredMute: !muteResponse.CurrentMute})
       break
     }
+
+    case 'repeatall':
+      await device.SetRepeat(Repeat.RepeatAll)
+      break
+    case 'repeatone':
+      await device.SetRepeat(Repeat.RepeatOne)
+      break
+    case 'repeatoff':
+      await device.SetRepeat(Repeat.Off)
+      break
+    case 'togglerepeat':
+      await toggleRepeat()
+      break
+    case 'shuffleon':
+      await device.SetShuffle(true)
+      break
+    case 'shuffleoff':
+      await device.SetShuffle(false)
+      break
+    case 'toggleshuffle':
+      await device.SetShuffle(!await device.GetShuffle())
+      break
     }
   }
 }
